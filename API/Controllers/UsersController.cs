79ec511cbb9a8ffc2607f140a58b61eca3393101
Arina4325 <arina4325@gmail.com>
2025-02-25 13:25:15 +0200

    AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,32 +1,37 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
+[Authorize]
 
-public class UsersController(DataContext context) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
 
     // API - endpoints
-    [AllowAnonymous]
+
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>>GetUsers()
     {
-        var users= await context.Users.ToListAsync();
-        return users;
+        var users= await userRepository.GetUsersAsync();
+        var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
+        return Ok(usersToReturn);
     }
 
-    [Authorize]
-    [HttpGet("{id:int}")]  // /api/users/id
-    public async Task<ActionResult<AppUser>>GetUser(int id)
+
+    [HttpGet("{username}")]  // /api/users/id
+    public async Task<ActionResult<MemberDto>>GetUser(string username)
     {
-        var user= await context.Users.FindAsync(id);
+        var user= await userRepository.GetUserByUsernameAsync(username);
 
         if (user == null) return NotFound();
-        return user;
+        return mapper.Map<MemberDto>(user);
     }
 
 }
