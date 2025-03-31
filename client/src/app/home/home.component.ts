9f3bf9655c493e6b1ec39d54d556dbd613c7f91b
Arina4325 <arina4325@gmail.com>
2025-03-31 15:07:59 +0300

    getting members from server

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,19 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient);
+export class HomeComponent {
   registerMode = false;
-  users: any;
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle(){
     this.registerMode = !this.registerMode
@@ -21,16 +15,6 @@ export class HomeComponent implements OnInit {
 
   cancelRegisterMode(event: boolean){
     this.registerMode = event;
-  }
-
-
-  getUsers(){
-    this.http.get('https://localhost:5026/api/users').subscribe({
-      next: response =>this.users=response,
-      error: error =>console.log(error),
-      complete: () =>console.log('Request has complited'),
-      
-    })
   }
 
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit{
+ private memberService = inject(MembersService);
+ members: Member[] = [];
+
+  ngOnInit(): void {
+    this.loadMembers();
+    
+  }
+
+  loadMembers(){
+    this.memberService.getMembers().subscribe({
+      next: members => this.members = members
+    })
+
+  }
+
+  trackByFn(index: number, member: any): number{
+    return member.id;
+  }
 
 }
